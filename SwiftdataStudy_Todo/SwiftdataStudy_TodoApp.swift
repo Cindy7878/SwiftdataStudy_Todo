9d3cc81b0d88bdd13d1d63b38183f757466da21e
Cindy7878 <cindy000708@gmail.com>
2024-05-22 17:33:32 +0900

    repository 연동

---
+++ SwiftdataStudy_Todo/SwiftdataStudy_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftdataStudy_TodoApp.swift
+//  SwiftdataStudy_Todo
+//
+//  Created by SOOKYUNG CHO on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftdataStudy_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
